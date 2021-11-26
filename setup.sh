1c07437e262fa201b4d82ec9c0a34f3135dae4a9
N <edaonh@gmail.com>
2021-11-26 13:21:37 +0300

    debug

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 
 cp ./files/* /opt/
+ansible-playbook cisco.yml
 ansible-playbook linux_db.yml
 ansible-playbook linux_ssl.yml
 ansible-playbook linux_ntp.yml
 ansible-playbook linux_nginx.yml
 ansible-playbook linux_docker.yml
-ansible-playbook cisco.yml
